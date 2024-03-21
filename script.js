75f3caf9bd855b0d161711b3e241b4eba9c15fe7
Ellis Burgess <ellismburges@gmail.com>
2024-03-21 14:53:33 +0000

    Improve DOM object handling

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const genButton = document.getElementById("generate-sim");
-const simInfo = document.getElementById("sim-info");
 const skinTones = ['light', 'medium light', 'medium dark', 'dark'];
 const hairColors = ['blonde', 'ginger', 'brown', 'black'];
 const eyeColors = ['light blue', 'dark blue', 'green', 'brown', 'grey'];
+
+let simInfo;
+clearPrevious();
 
 function generateAppearance() {
   let skin = skinTones[Math.floor(Math.random() * 4)]; 
@@ -13,10 +15,12 @@ function generateAppearance() {
 }
 
 function clearPrevious() {
-  let prevGenInfo = document.getElementById('generated-info');
-  if (prevGenInfo != null) {
-    simInfo.removeChild(prevGenInfo);
+  if (typeof simInfo !== 'undefined') {
+    simInfo.remove();
   }
+  simInfo = document.createElement("article");
+  simInfo.id = "sim-info";
+  document.body.appendChild(simInfo);
 }
 
 function generateSim() {
