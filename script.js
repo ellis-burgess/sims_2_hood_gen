ee048403b3731238d2a4b1af183f2752e2733cfc
Ellis Burgess <ellismburges@gmail.com>
2024-06-05 15:05:33 +0100

    Removed obsolete script

--- script.js
+++ script.js
@@ -1,152 +1,125 @@
-const turn_ons = await fetch('./turn_ons.json');
-const turn_ons_data = await turn_ons.json();
-console.log(turn_ons_data);
+// Import turn ons and turn offs from json file
+const turnOns = await fetch('./turn_ons.json');
+const turnOnsData = await turnOns.json();
+console.log(turnOnsData);
 
-const genButton = document.getElementById("generate-sim");
-const resetButton = document.getElementById("clear-sims");
+// API URL
+const nameApiUrl = 'https://randomuser.me/api/?nat=au,br,ca,ch,de,dk,es,fi,fr,gb,ie,mx,nl,nz,us';
+
+// Sim variables
 const skinTones = ['light', 'medium light', 'medium dark', 'dark'];
 const hairColors = ['blonde', 'ginger', 'brown', 'black'];
 const eyeColors = ['light blue', 'dark blue', 'green', 'brown', 'grey'];
 const weights = ['medium weight', 'heavy weight'];
 const personalityTraits = ['Sloppy/Neat', 'Shy/Outgoing', 'Lazy/Active', 'Serious/Playful', 'Grouchy/Nice'];
-const nameApiUrl = 'https://randomuser.me/api/?nat=au,br,ca,ch,de,dk,es,fi,fr,gb,ie,mx,nl,nz,us';
 
-let simInfo;
-clearPrevious();
+// DOM Elements
+const genButton = document.getElementById("generate-sim");
 
-function generateAppearance() {
-  let skin = skinTones[Math.floor(Math.random() * 4)]; 
-  let hair = hairColors[Math.floor(Math.random() * 4)];
-  let eye = eyeColors[Math.floor(Math.random() * 5)];
-  let weight = weights[Math.floor(Math.random() * 2)];
+// Sim class
 
-  return [`Skin tone: ${skin}`, `Hair color: ${hair}`, `Eye color: ${eye}`, `${weight}`];
-}
+class Sim {
+  constructor(name, appearance, personality, attractions) {
+    this.firstName = name['firstName'];
+    this.surname = name['secondName'];
+    this.gender = name['gender'];
 
-function clearPrevious() {
-  if (typeof simInfo !== 'undefined') {
-    simInfo.remove();
+    this.skintone = appearance['skin'];
+    this.hairColor = appearance['hairColor'];
+    this.eyeColor = appearance['eyeColor'];
+    this.weight = appearance['weight'];
+
+    this.personality = personality;
+
+    this.attractions = attractions;
   }
-  simInfo = document.createElement("article");
-  simInfo.id = "sim-info";
-  document.body.appendChild(simInfo);
 }
 
+// Generate name using API
 async function getName() {
   const response = await fetch(nameApiUrl);
   const data = await response.json();
   let first = (data["results"][0]["name"]["first"]);
   let second = (data["results"][0]["name"]["last"]);
   let gender = (data["results"][0]["gender"]);
-  return `${first} ${second} (${gender})`;
+
+  return {'firstName': first, 'secondName': second, 'gender': gender};
 }
 
+// Generate appearance (skintone, hair colour, eye colour, weight)
+function setAppearance() {
+  let skin = skinTones[Math.floor(Math.random() * 4)]; 
+  let hair = hairColors[Math.floor(Math.random() * 4)];
+  let eye = eyeColors[Math.floor(Math.random() * 5)];
+  let weight = weights[Math.floor(Math.random() * 2)];
+
+  return {'skin': skin, 'hairColor': hair, 'eyeColor': eye, 'weight': weight};
+}
+
+// Generate personality
 function setPersonality() {
   let personality = [5, 5, 5, 5, 5];
   let randamount = Math.floor(Math.random() * 50) + 50;
   for (let i = 0; i < randamount; i++) {
     let r1 = 0;
     let r2 = 0;
+    let strength = Math.floor(Math.random() * 3) + 1
 
-    while (r1 == r2 || personality[r1] == 0 || personality[r2] == 10) {
+    while (r1 == r2 || personality[r1] < strength || personality[r2] > (10 - strength)) {
       r1 = Math.floor(Math.random() * 5);
       r2 = Math.floor(Math.random() * 5);  
     }
 
-    personality[r1] -= 1;
-    personality[r2] += 1;
+    personality[r1] -= strength;
+    personality[r2] += strength;
   }
-  return personality;
+
+  let personalityObj = {}
+  for (let i = 0; i < personality.length; i++) {
+    personalityObj[personalityTraits[i]] = personality[i];
+  }
+
+  return personalityObj;
 }
 
+// Generate attractions
 function setAttraction() {
   // Determine categories for turn-ons and turn-off
-  let turn_on_categories = [Math.floor(Math.random() * 4)];
-  while (turn_on_categories.length < 3) {
-    let cat = Math.floor(Math.random() * 4);
-    if (!turn_on_categories.includes(cat)) {
-      turn_on_categories.push(cat);
+  let turnOnKeys = Object.keys(turnOnsData);
+  let turnOnCategories = [Math.floor(Math.random() * turnOnKeys.length)];
+  while (turnOnCategories.length < 3) {
+    let cat = Math.floor(Math.random() * turnOnKeys.length);
+    if (!turnOnCategories.includes(cat)) {
+      turnOnCategories.push(cat);
     }
   }
 
   let selections = [];
 
-  for (const cat of turn_on_categories) {
-    let options = turn_ons_data[Object.keys(turn_ons_data)[cat]];
-    let option_type = "";
+  for (const cat of turnOnCategories) {
+    let options = turnOnsData[turnOnKeys[cat]];
     let selection = ""
     if (options instanceof Array) {
-      option_type = "array";
       selection = options[Math.floor(Math.random() * options.length)];
     } else {
-      option_type = "object";
-      let option_keys = Object.keys(options);
-      selection = options[option_keys[Math.floor(Math.random() * option_keys.length)]];
+      let optionKeys = Object.keys(options);
+      selection = options[optionKeys[Math.floor(Math.random() * optionKeys.length)]];
       selection = selection[Math.floor(Math.random() * selection.length)];
     }
-    selections.push(selection)
-  }
-  return selections;
-}
-
-function createSectionDisplay(title, className, subheadings = null) {
-  let container = document.createElement("section");
-  container.classList.add(className);
-  let containerTitle = document.createElement("h3");
-  containerTitle.innerText = title;
-  container.appendChild(containerTitle);
-  if (subheadings != null) {
-    for (let i = 0; i < subheadings.length; i++) {
-      let subList = document.createElement("ul");
-      subList.classList.add(subheadings[i].toLowerCase());
-      let subListTitle = document.createElement("h4");
-      subListTitle.innerText = subheadings[i];
-      container.appendChild(subListTitle);
-      container.appendChild(subList);
-    }
-  } else {
-    let list = document.createElement("ul");
-    container.appendChild(list)
+    selections.push(selection);
   }
 
-  return container;
+  return {'turnOns': [selections[0], selections[1]], 'turnOffs': selections[2]};
 }
 
-function createSimDisplay(parent) {
-  let appearanceContainer = createSectionDisplay("Appearance", "appearance-info");
-  let personalityContainer = createSectionDisplay("Personality", "personality-info");
-  let attractionContainer = createSectionDisplay("Turn-Ons and Turn-Offs", "attraction-info", ["Turn-ons", "Turn-offs"]);
-
-  parent.appendChild(appearanceContainer);
-  parent.appendChild(personalityContainer);
-  parent.appendChild(attractionContainer);
-
-  return [appearanceContainer.getElementsByTagName('ul')[0], personalityContainer.getElementsByTagName('ul')[0]];
-}
 
 async function generateSim() {
-  let name = document.createElement("h2");
-  name.innerText = await getName();
-  simInfo.appendChild(name);
-
-  let displayLists = createSimDisplay(simInfo);
-
-  let appearance = generateAppearance();
-  for (let i = 0; i < appearance.length; i++) {
-    let node = document.createElement("li");
-    node.innerText = appearance[i];
-    displayLists[0].appendChild(node);
-  }
-
-  let personality = setPersonality();
-  for (let i = 0; i < personality.length; i++) {
-    let node = document.createElement("li");
-    node.innerText = `${personalityTraits[i]}: ${personality[i]}`;
-    displayLists[1].appendChild(node);
-  }
-
-  setAttraction();
+  let name = (await getName());
+  let appearance = (setAppearance());
+  let personality = (setPersonality());
+  let attraction = (setAttraction());
+  let newSim = new Sim(name, appearance, personality, attraction);
+  console.log(newSim);
 }
 
 genButton.addEventListener("click", generateSim);
-resetButton.addEventListener("click", clearPrevious);
