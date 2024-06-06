7653ee7b62355112d3af90a3f258187ce74d36bd
ellis-burgess <ellismburges@gmail.com>
2024-06-06 11:47:04 +0100

    Replace tutorial code with own code

--- carousel.js
+++ carousel.js
@@ -1,14 +1,16 @@
-const track = document.querySelector('.carousel__track');
+const track = document.querySelector('.carousel__contents');
 const slides = Array.from(track.children);
-const nextButton = document.querySelector('.carousel__button--right');
-const prevButton = document.querySelector('.carousel__button--left');
+console.log(slides);
 
-let dotsNav = document.querySelector('.carousel__nav');
-let dots = [];
+const nextButton = document.querySelector('.carousel__button-right');
+const prevButton = document.querySelector('.carousel__button-left');
+
+const dotsNav = document.querySelector('.carousel__nav');
+const dots = []
+console.log(dots);
 
 const slideWidth = slides[0].getBoundingClientRect().width;
 
-// arrange slides next to one another
 const setSlidePosition = (slide, index) => {
   slide.style.left = slideWidth * index + 'px';
 };
@@ -29,71 +31,48 @@ const addDot = (slide, index) => {
 slides.forEach(setSlidePosition);
 slides.forEach(addDot);
 
-// when I click left, move slides to the left
-prevButton.addEventListener('click', e => {
-  const currentSlide = track.querySelector('.current-slide');
-  let prevSlide = currentSlide.previousElementSibling;
-  if (!prevSlide) {
-    prevSlide = track.lastElementChild;
-  }
+nextButton.addEventListener('click', slideOne);
+prevButton.addEventListener('click', slideOne);
 
-  const currentDot = dotsNav.querySelector('.current-slide');
-  let prevDot = currentDot.previousElementSibling;
-  if (!prevDot) {
-    prevDot = dotsNav.lastElementChild;
-  }
-  const amountToMove = prevSlide.style.left;
-  // move to the next slide
-  track.style.transform = 'translateX(-' + amountToMove + ')';
-  currentSlide.classList.remove('current-slide');
-  prevSlide.classList.add('current-slide');
-  currentDot.classList.remove('current-slide');
-  prevDot.classList.add('current-slide');
-})
-
-// when I click right, move slides to the right
-nextButton.addEventListener('click', e => {
-  const currentSlide = track.querySelector('.current-slide');
-  let nextSlide = currentSlide.nextElementSibling;
-  if (!nextSlide) {
-    nextSlide = track.firstElementChild;
-  }
-
-  const currentDot = dotsNav.querySelector('.current-slide');
-  let nextDot = currentDot.nextElementSibling;
-  if (!nextDot) {
-    nextDot = dotsNav.firstElementChild;
-  }
-  const amountToMove = nextSlide.style.left;
-  // move to the next slide
-  track.style.transform = 'translateX(-' + amountToMove + ')';
-  currentSlide.classList.remove('current-slide');
-  nextSlide.classList.add('current-slide');
-  currentDot.classList.remove('current-slide');
-  nextDot.classList.add('current-slide');
-})
-
-// when I click nav indicators, move to that slide
-
-dots.forEach(function(dot) {
-  dot.addEventListener("click", e => {
-    // on click event
-    let currentDot = dotsNav.querySelector('.current-slide');
-    let nextDot = e.target;
-        
-    if (currentDot == nextDot) {
-      return 0;
+function slideOne(e) {
+  let currentSlide = track.querySelector('.current-slide');
+  let currentDot = dotsNav.querySelector('.current-slide');
+  let nextSlide = null;
+  if (e.target.classList.contains('carousel__button-right')) {
+    nextSlide = currentSlide.nextElementSibling;
+    if (!nextSlide) {
+      nextSlide = slides[0];
     }
-    
-    let currentSlide = track.querySelector('.current-slide');
-    let nextSlide = slides[dots.indexOf(nextDot)];
-    
-    const amountToMove = nextSlide.style.left;
-    // move to the next slide
-    track.style.transform = 'translateX(-' + amountToMove + ')';
-    currentSlide.classList.remove('current-slide');
-    nextSlide.classList.add('current-slide');
-    currentDot.classList.remove('current-slide');
-    nextDot.classList.add('current-slide');
-  })
-})
+  } else {
+    nextSlide = currentSlide.previousElementSibling
+    if (!nextSlide) {
+      nextSlide = slides[slides.length-1];
+    }
+  }
+  
+  let nextDot = dots[slides.indexOf(nextSlide)];
+  
+  moveSlides(currentSlide, currentDot, nextSlide, nextDot);
+}
+
+// buttons jump to relevant carousel item
+
+function jumpToDot(e) {
+  let currentDot = dotsNav.querySelector('.current-slide');
+  let nextDot = e.target;
+  
+  let currentSlide = track.querySelector('.current-slide');
+  let nextSlide = slides[dots.indexOf(nextDot)];
+  
+  moveSlides(currentSlide, currentDot, nextSlide, nextDot);
+}
+
+function moveSlides(currentSlide, currentDot, nextSlide, nextDot) {
+  let amountToMove = nextSlide.style.left;
+  track.style.transform = `translateX(-${amountToMove})`;
+  currentSlide.classList.remove('current-slide');
+  currentDot.classList.remove('current-slide');
+  
+  nextSlide.classList.add('current-slide');
+  nextDot.classList.add('current-slide');
+}
